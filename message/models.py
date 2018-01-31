aaec5e1084b316ee5c6ad99c36aea362677b4ded
sty945 <sutaoyu@yeah.net>
2018-01-31 23:41:54 +0800

    新建数据库的表

--- message/models.py
+++ message/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u'用户名')
+    email = models.EmailField(verbose_name=u'邮箱')
+    address = models.CharField(max_length=100, verbose_name=u'邮箱地址')
--- message/views.py
+++ message/views.py
@@ -1,5 +1,7 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
+#Django request 对象
 def getform(request):
     return render(request, 'message_form.html')
